bde844cb27b76b6fe484a0ec958fa41a3800b0ec
kamilakrygier <kamilakrygier2308@gmail.com>
2022-11-23 01:53:49 +0100

    Added a second version of a program that will calculate the factorial of a number

---
+++ CalculateTheFactorialOfANumberVol2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int factorial(int number)
+{
+  if (number == 1)
+  {
+    return number;
+  }
+  return number * factorial(number - 1);
+}
+
+int main()
+{
+   std::cout << "Welcome to the program that will calculate the factorial of a number!\n";
+   
+   int number = 0;
+   
+   std::cout << "Enter number You would like to calculate the factorial from: ";
+   std::cin >> number;
+   
+   std::cout << factorial(number) << std::endl;
+   
+   return 0;
+}
