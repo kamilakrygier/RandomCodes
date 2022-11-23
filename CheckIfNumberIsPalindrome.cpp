874d94f5abd33dee0383ac8b6c5f19af77ccb426
kamilakrygier <kamilakrygier2308@gmail.com>
2022-11-23 01:58:35 +0100

    Added a new program that will check if number is palindrome

---
+++ CheckIfNumberIsPalindrome.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+bool isNumberPalindrome(std::string& number, std::string& numberReverse)
+{
+  if (number == numberReverse)
+  {
+    return true;
+  }
+  return false;
+}
+
+void reverseNumber(std::string& numberReverse)
+{
+  std::reverse(numberReverse.begin(), numberReverse.end());
+}
+
+int main()
+{
+  std::cout << "Welcome to the program that will check if your number is palindrome!\n";
+
+  std::string number = "";
+
+  std::cout << "Please write a number, You would like to check: ";
+  std::cin >> number;
+  
+  std::string numberReverse = number;
+  
+  reverseNumber(numberReverse);
+  
+  std::cout << std::boolalpha << isNumberPalindrome(number, numberReverse) << std::endl;
+
+  return 0;
+}
