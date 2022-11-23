bf267af1c597a26dfeda5071cebe59389a055e4e
kamilakrygier <kamilakrygier2308@gmail.com>
2022-11-23 01:56:40 +0100

    Added a third version of a program that will change letters to uppercase

---
+++ ChangeToUppercaseVol3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+
+std::string changingWordToUppercase(std::string& word)
+{
+  for (auto& it : word)
+  {
+    if (islower(it))
+    {
+      it -= 32;
+    }
+  }
+  return word;
+}
+int main()
+{
+  std::cout << "Welcome to the program that will change size of the letters in word - from lowercase to uppercase\n";
+  
+  std::string word = "";
+  
+  std::cout << "Enter your word: ";
+  std::cin >> word;
+  
+  std::cout << changingWordToUppercase(word) << std::endl;
+  
+  return 0;
+}
